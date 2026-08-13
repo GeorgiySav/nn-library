badee37c24d53218dce9370edaa532ded59ad90f
Georgiy Savchenko <gesav@live.com>
2026-08-13 23:13:01 +0100

    added shape

---
+++ include/nn/core/shape.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <initializer_list>
+#include <cassert>
+#include <string>
+
+namespace nn {
+
+inline constexpr int kMaxShapeRank = 8;
+class Shape {
+public:
+  Shape() = default;
+  Shape(std::initializer_list<int> dims) {
+    assert(dims.size() <= kMaxShapeRank);
+    rank_ = dims.size();
+    int i = 0;
+    for (const auto& d : dims) {
+      dims_[i++] = d;
+    }
+  }
+
+  int rank() const { return rank_; }
+  int dim(int i) const {
+    assert(i < rank_);
+    return dims_[i];
+  }
+
+  int numel() const {
+    int n = 1;
+    for (int i = 0; i < rank_; ++i) {
+      n *= dims_[i];
+    }
+    return n;
+  }
+
+  bool operator==(const Shape& other) const {
+    if (rank_ != other.rank_) {
+      return false;
+    }
+    for (int i = 0; i < rank_; ++i) {
+      if (dims_[i] != other.dims_[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool operator!=(const Shape& other) const {
+    return !(*this == other);
+  }
+
+  std::string str() const {
+    std::string s = "[";
+    for (int i = 0; i < rank_; ++i) {
+      if (i > 0) {
+        s += ", ";
+      }
+      s += std::to_string(dims_[i]);
+    }
+    s += "]";
+    return s;
+  }
+
+private:
+  int dims_[kMaxShapeRank] = {0};
+  int rank_ = 0;
+};
+
+}
--- tests/test_core_primitives.cpp
+++ tests/test_core_primitives.cpp
@@ -1,6 +1,7 @@
 #include "test_harness.h"
 
 #include <nn/core/small_vec.h>
+#include <nn/core/shape.h>
 
 NN_TEST(test_small_vec) {
   nn::SmallVec<int, 4> v;
@@ -25,4 +26,21 @@ NN_TEST(test_small_vec) {
   NN_CHECK(v.size() == 3);
 
   NN_CHECK(std::equal(v.begin(), v.end(), std::vector<int>{1, 2, 3}.begin()));
+}
+
+NN_TEST(test_shape) {
+  nn::Shape s1{2, 3, 4};
+  NN_CHECK(s1.rank() == 3);
+  NN_CHECK(s1.dim(0) == 2);
+  NN_CHECK(s1.dim(1) == 3);
+  NN_CHECK(s1.dim(2) == 4);
+  NN_CHECK(s1.numel() == 24);
+
+  nn::Shape s2{2, 3, 4};
+  nn::Shape s3{2, 3, 5};
+  NN_CHECK(s1 == s2);
+  NN_CHECK(s1 != s3);
+
+  std::string str = s1.str();
+  NN_CHECK(str == "[2, 3, 4]");
 }
